0575d6913272c8275c575feb29b1b0600fd95bb3
Domen Koder <domen.koder@gmail.com>
2022-05-01 19:21:22 +0200

    Program finished, using 4 variables.

--- Program.cs
+++ Program.cs
@@ -1,7 +1,25 @@
 ﻿int a;
 int b;
-int swap;
+int swapA;
+int swapB;
+
+a = 1;
+b = 2;
+swapA = 0;
+swapB = 0;
+
+//Temporarily save the value of a
+swapA = a;
+
+//Temporarily save the value of b
+swapB = b;
 
 
+//a takes the value of b
+a = b;
 
-    
+//b takes the value of a
+b = swapA;
+
+Console.WriteLine(a);
+Console.WriteLine(b);
